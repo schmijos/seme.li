744f0cb7776b6fccf54afffda5d496090d47182b
Josua Schmid <josua.schmid@renuo.ch>
2023-01-24 21:44:10 +0100

    Convenience configurations

--- src/semel.cr
+++ src/semel.cr
@@ -2,6 +2,9 @@ require "kemal"
 require "./storage"
 
 serve_static false
+static_headers do |response, filepath, filestat|
+  response.headers.add("Access-Control-Allow-Origin", "*")
+end
 
 storage = Storage.new(ENV["KEMAL_ENV"]? || "development")
 
@@ -20,7 +23,7 @@ post "/create" do |env|
   expires_in_seconds = env.params.json["expires_in_seconds"].as(Int64)
   secret_id = storage.put(ciphertext, Time.utc.to_unix + expires_in_seconds)
 
-  { url: "https://seme.li/try##{secret_id}" }.to_json
+  { url: "https://#{env.request.headers["Host"]}/try##{secret_id}" }.to_json
 end
 
 delete "/consume/:id" do |env|
@@ -33,6 +36,16 @@ delete "/consume/:id" do |env|
   env.response.status_code = 400 unless ciphertext # actually 404, but Kemal then renders HTML?!
 
   { ciphertext: ciphertext }.to_json
+end
+
+get "/robots.txt" do |env|
+  env.response.content_type = "text/plain; charset=utf-8"
+
+  <<-STRING
+  user-agent: *
+  Allow: /$
+  Disallow: /
+  STRING
 end
 
 Kemal.run
